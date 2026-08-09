674c8a6d74478df695ca8d8d4ade26efe3aef272
Shivang Jaiswal <jaiswalshivang01@gmail.com>
2026-08-09 10:49:08 +0530

    creating 50/50 chances

--- flaky_test.go
+++ flaky_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestFlakyNetworkCall(t *testing.T) {
-	if rand.Float64() < 0.3 {
+	if rand.Float64() < 0.5 {
 		time.Sleep(100 * time.Millisecond)
 		t.Fatalf("connection to auth-service timed out after 2000ms")
 	}
